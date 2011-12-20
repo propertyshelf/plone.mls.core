84c3a9832f76cbd97f3fa993416ee6f666146fda
Thomas Massmann <thomas@propertyshelf.com>
2011-12-20 20:36:14 +0100

    Replaced old PloneTestCase based tests with plone.app.testing.

--- src/plone/mls/core/testing.py
+++ src/plone/mls/core/testing.py
@@ -19,10 +19,32 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 ###############################################################################
-"""Plone support for the Propertyshelf MLS"""
+"""Test Layer for plone.mls.core."""
 
 # zope imports
-from zope.i18nmessageid import MessageFactory
+from plone.app.testing import (IntegrationTesting, PloneSandboxLayer,
+    PLONE_FIXTURE, applyProfile)
+from zope.configuration import xmlconfig
 
 
-_ = MessageFactory('plone.mls.core')
+class PloneMLSCore(PloneSandboxLayer):
+    """Custom Test Layer for plone.mls.core."""
+    defaultBases = (PLONE_FIXTURE, )
+
+    def setUpZope(self, app, configurationContext):
+        """Set up Zope for testing."""
+        # Load ZCML
+        import plone.mls.core
+        xmlconfig.file('configure.zcml', plone.mls.core,
+                       context=configurationContext)
+
+    def setUpPloneSite(self, portal):
+        """Set up a Plone site for testing."""
+        applyProfile(portal, 'plone.mls.core:default')
+
+
+PLONE_MLS_CORE_FIXTURE = PloneMLSCore()
+PLONE_MLS_CORE_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(PLONE_MLS_CORE_FIXTURE, ),
+    name='PloneMLSCore:Integration',
+)
--- src/plone/mls/core/tests/test_controlpanel.py
+++ src/plone/mls/core/tests/test_controlpanel.py
@@ -1,26 +1,32 @@
 # -*- coding: utf-8 -*-
 
-##############################################################################
+###############################################################################
 #
-# Copyright (c) 2011 Propertyshelf, LLC and Contributors.
+# Copyright (c) 2011 Propertyshelf, Inc. and its Contributors.
 # All Rights Reserved.
 #
-# This software is subject to the provisions of the Zope Public License,
-# Version 2.1 (ZPL). A copy of the ZPL should accompany this distribution.
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by the
+# Free Software Foundation.
 #
-##############################################################################
-"""Test the control panel for plone.mls.core."""
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+###############################################################################
+"""Test Control Panel for plone.mls.core."""
 
 # python imports
-import unittest
+import unittest2 as unittest
 
 # zope imports
 from Products.CMFCore.utils import getToolByName
-from Products.PloneTestCase.ptc import PloneTestCase
+from plone.app.testing import TEST_USER_ID, logout, setRoles
 from plone.registry import Registry
 from zope.component import getMultiAdapter
 from zope.interface import directlyProvides
@@ -28,53 +34,37 @@ from zope.interface import directlyProvides
 # local imports
 from plone.mls.core.browser.interfaces import IMLSSpecific
 from plone.mls.core.interfaces import IMLSSettings
-from plone.mls.core.tests.layer import MLSCoreLayer
+from plone.mls.core.testing import PLONE_MLS_CORE_INTEGRATION_TESTING
 
 
-class RegistryTest(PloneTestCase):
+class TestMLSControlPanel(unittest.TestCase):
+    """Control Panel Test Case for plone.mls.core."""
+    layer = PLONE_MLS_CORE_INTEGRATION_TESTING
 
-    layer = MLSCoreLayer
-
-    def afterSetUp(self):
+    def setUp(self):
+        """Additional test setup."""
+        self.portal = self.layer['portal']
         directlyProvides(self.portal.REQUEST, IMLSSpecific)
-        self.loginAsPortalOwner()
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
         self.registry = Registry()
         self.registry.registerInterface(IMLSSettings)
 
     def test_mls_controlpanel_view(self):
+        """Test that the MLS configuration view is available."""
         view = getMultiAdapter((self.portal, self.portal.REQUEST),
-            name="mls-controlpanel")
+                               name='mls-controlpanel')
         view = view.__of__(self.portal)
-        self.failUnless(view())
+        self.assertTrue(view())
 
     def test_mls_controlpanel_view_protected(self):
-        # Test that the MLS setting control panel view can not be accessed
-        # by anonymous users.
+        """Test that the MLS configuration view needs authentication."""
         from AccessControl import Unauthorized
-        self.logout()
+        logout()
         self.assertRaises(Unauthorized, self.portal.restrictedTraverse,
-            '@@mls-controlpanel')
+                          '@@mls-controlpanel')
 
     def test_mls_in_controlpanel(self):
-        # Check that there is an MLS entry in the control panel.
-        self.controlpanel = getToolByName(self.portal, "portal_controlpanel")
-        self.failUnless('propertyshelf_mls' in [a.getAction(self)['id']
+        """Check that there is an MLS entry in the control panel."""
+        self.controlpanel = getToolByName(self.portal, 'portal_controlpanel')
+        self.assertTrue('propertyshelf_mls' in [a.getAction(self)['id']
             for a in self.controlpanel.listActions()])
-
-    def test_record_mls_key(self):
-        # Test that the mls_key record is in the control panel.
-        record_mls_key = self.registry.records[
-            'plone.mls.core.interfaces.IMLSSettings.mls_key']
-        self.failUnless('mls_key' in IMLSSettings)
-        self.assertEquals(record_mls_key.value, u"")
-
-    def test_record_mls_site(self):
-        # Test that the mls_site record is in the control panel.
-        record_mls_site = self.registry.records[
-            'plone.mls.core.interfaces.IMLSSettings.mls_site']
-        self.failUnless('mls_site' in IMLSSettings)
-        self.assertEquals(record_mls_site.value, u"")
-
-
-def test_suite():
-    return unittest.defaultTestLoader.loadTestsFromName(__name__)
