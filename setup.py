4ed74098c55d5b45c360884b0d5c8a05815a06cf
Thomas Massmann <thomas@propertyshelf.com>
2013-06-27 12:55:56 +0200

    Prepared 0.4.2 release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-version = '0.5dev'
+version = '0.4.2'
 description = "Plone support for the Propertyshelf MLS."
 long_description = open("README.rst").read() + "\n" + open("CHANGES.rst").read()
 
