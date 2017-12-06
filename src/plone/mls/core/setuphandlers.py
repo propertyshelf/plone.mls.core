b9035fc2764be5b439aed91e0e400019c904a23b
Thomas Massmann <thomas@propertyshelf.com>
2017-12-06 13:38:04 -0500

    Hide install profile.

--- src/plone/mls/core/setuphandlers.py
+++ src/plone/mls/core/setuphandlers.py
@@ -32,6 +32,7 @@ class HiddenProfiles(object):
     def getNonInstallableProfiles(self):
         """Hide uninstall profile from site-creation and quickinstaller."""
         return [
+            'plone.mls.core:default',
             'plone.mls.core:install-base',
             'plone.mls.core:uninstall',
             'plone.mls.core:uninstall-base',
