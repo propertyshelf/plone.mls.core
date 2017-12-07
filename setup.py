ead9694fdaa5ceb4b6bb690a2e4d097a152e19a9
Thomas Massmann <thomas@propertyshelf.com>
2017-12-06 19:15:00 -0500

    No Plone 4.0 support anymore. [ci skip]

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Framework :: Plone",
-        "Framework :: Plone :: 4.0",
         "Framework :: Plone :: 4.1",
         "Framework :: Plone :: 4.2",
         "Framework :: Plone :: 4.3",
